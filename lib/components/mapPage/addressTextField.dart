fb86e579a020a7035c6a8803444062ddeac70812
here-comes-ivy <139777954+here-comes-ivy@users.noreply.github.com>
2024-09-05 16:32:41 +0800

    Updating addressTextField.dart

--- lib/components/mapPage/addressTextField.dart
+++ lib/components/mapPage/addressTextField.dart
@@ -2,15 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-class AddressTextFiled extends StatefulWidget {
+class AddressTextField extends StatefulWidget {
+  final String apiKey;
+  final String country;
+  final Function(String)? onAddressSelected;
+  final InputDecoration? decoration;
+
+  const AddressTextField({
+    Key? key,
+    required this.apiKey,
+    this.country = 'tw',
+    this.onAddressSelected,
+    this.decoration,
+  }) : super(key: key);
+
   @override
-  _AddressTextFiledState createState() => _AddressTextFiledState();
+  _AddressTextFieldState createState() => _AddressTextFieldState();
 }
 
-class _AddressTextFiledState extends State<AddressTextFiled> {
-
+class _AddressTextFieldState extends State<AddressTextField> {
   List<dynamic> predictions = [];
-  String googApiKey = 'AIzaSyBvCYfs_gzMM3iKU1NpW2XTOlPuwG13b1s';
+  TextEditingController _controller = TextEditingController();
 
   Future<void> _onSearchChanged(String searchText) async {
     if (searchText.isEmpty) {
@@ -21,27 +33,56 @@ class _AddressTextFiledState extends State<AddressTextFiled> {
     }
 
     String url =
-        'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$searchText&key=$googApiKey&components=country:tw';
-
-    final response = await http.get(Uri.parse(url));
-
-    if (response.statusCode == 200) {
-      setState(() {
-        predictions = json.decode(response.body)['predictions'];
-      });
-    } else {
-      throw Exception('Failed to load predictions');
+        'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$searchText&key=${widget.apiKey}&components=country:${widget.country}';
+    
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        setState(() {
+          predictions = json.decode(response.body)['predictions'];
+        });
+      } else {
+        print('Failed to load predictions: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error fetching predictions: $e');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      onChanged: _onSearchChanged,
-      decoration: InputDecoration(
-        hintText: 'Search places...',
-        border: OutlineInputBorder(),
-      ),
+    return Column(
+      children: [
+        TextField(
+          controller: _controller,
+          onChanged: _onSearchChanged,
+          decoration: widget.decoration ?? InputDecoration(
+            hintText: 'Search places...',
+            border: OutlineInputBorder(),
+          ),
+        ),
+        if (predictions.isNotEmpty)
+          Container(
+            height: 200, // 可以根据需要调整高度
+            child: ListView.builder(
+              itemCount: predictions.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(predictions[index]['description']),
+                  onTap: () {
+                    setState(() {
+                      _controller.text = predictions[index]['description'];
+                      predictions = []; // 清空预测列表
+                    });
+                    if (widget.onAddressSelected != null) {
+                      widget.onAddressSelected!(_controller.text);
+                    }
+                  },
+                );
+              },
+            ),
+          ),
+      ],
     );
   }
 }
