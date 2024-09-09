df681e685251b5c7077570f21a9aa304b2d9986f
Ivy <ivyhsu0919@gmail.com>
2024-09-09 11:25:16 +0800

    updating address autocompletion

--- lib/components/mapPage/addressAutoCompleteTextField.dart
+++ lib/components/mapPage/addressAutoCompleteTextField.dart
@@ -3,51 +3,33 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'filter_filterBottomSheet.dart';
+import 'package:restaurant_picker/services/addressAutoCompletion.dart';
+import 'package:restaurant_picker/services/mapFilterProvider.dart';
+import 'package:restaurant_picker/services/locationDataProvider.dart';
 
 
 class AddressAutoCompleteTextField extends StatefulWidget {
-  final String apiKey = dotenv.env['googApikey']!;
-  final String country;
   final Function(String)? onAddressSelected;
-  final InputDecoration? decoration;
 
-  AddressAutoCompleteTextField({
-    Key? key,
-    this.country = 'tw',
-    this.onAddressSelected,
-    this.decoration,
-  }) : super(key: key);
+  AddressAutoCompleteTextField({this.onAddressSelected});
 
   @override
   _AddressAutoCompleteTextFieldState createState() => _AddressAutoCompleteTextFieldState();
 }
 
 class _AddressAutoCompleteTextFieldState extends State<AddressAutoCompleteTextField> {
-  List<dynamic> predictions = [];
+  Future<List<String>>? _predictions;
   TextEditingController _controller = TextEditingController();
 
-  Future<void> _onSearchChanged(String searchText) async {
-    if (searchText.isEmpty) {
+  void _onSearchChanged(String searchText) {
+    if (searchText.isNotEmpty) {
       setState(() {
-        predictions = [];
+        _predictions = AddressAutoCompletion().getPlacesAutocomplete(input: searchText);
       });
-      return;
-    }
-
-    String url =
-        'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$searchText&key=${widget.apiKey}&components=country:${widget.country}';
-    
-    try {
-      final response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        setState(() {
-          predictions = json.decode(response.body)['predictions'];
-        });
-      } else {
-        print('Failed to load predictions: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('Error fetching predictions: $e');
+    } else {
+      setState(() {
+        _predictions = null;
+      });
     }
   }
 
@@ -58,58 +40,72 @@ class _AddressAutoCompleteTextFieldState extends State<AddressAutoCompleteTextFi
         TextField(
           style: TextStyle(color: Colors.black),
           decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.white,
-                            prefixIcon: Icon(Icons.search, color: Colors.grey),
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                Icons.tune,
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .surface
-                                    .withOpacity(0.8),
-                              ),
-                              onPressed: () {
-                                FilterBottomSheet.show(context);
-                              },
-                            ),
-                            hintText: 'Search on Map',
-                            hintStyle: TextStyle(color: Colors.grey),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(8.0),
-                              ),
-                              borderSide: BorderSide.none,
-                            ),
-                          ),
+            filled: true,
+            fillColor: Colors.white,
+            prefixIcon: Icon(Icons.search, color: Colors.grey),
+            suffixIcon: IconButton(
+              icon: Icon(
+                Icons.tune,
+                color: Theme.of(context).colorScheme.surface,
+              ),
+              onPressed: () {
+                FilterBottomSheet.show(context);
+              },
+            ),
+            hintText: 'Search on Map',
+            hintStyle: TextStyle(color: Colors.grey),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(8.0),
+              ),
+              borderSide: BorderSide.none,
+            ),
+          ),
           controller: _controller,
           onChanged: _onSearchChanged,
         ),
-        if (predictions.isNotEmpty)
-          Container(
-            color: Colors.white,
-            height: 200, 
-            child: ListView.builder(
-              itemCount: predictions.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(predictions[index]['description'], style: TextStyle(color: Theme.of(context)
-                                    .colorScheme
-                                    .surface),),
-                  onTap: () {
-                    setState(() {
-                      _controller.text = predictions[index]['description'];
-                      predictions = []; 
-                    });
-                    if (widget.onAddressSelected != null) {
-                      widget.onAddressSelected!(_controller.text);
-                    }
-                  },
+        if (_predictions != null)
+          FutureBuilder<List<String>>(
+            future: _predictions,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                print('Error: ${snapshot.error}');
+              
+                return Container();
+              } else if (snapshot.hasData) {
+                final predictions = snapshot.data!;
+                return Container(
+                  color: Colors.white,
+                  height: 200,
+                  child: ListView.builder(
+                    itemCount: predictions.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(
+                          predictions[index],
+                          style: TextStyle(color: Theme.of(context).colorScheme.surface),
+                        ),
+                        onTap: () {
+                          setState(() {
+                            _controller.text = predictions[index];
+                            _predictions = null;
+                          });
+                          if (widget.onAddressSelected != null) {
+                            widget.onAddressSelected!(_controller.text);
+                          }
+                        },
+                      );
+                    },
+                  ),
                 );
+              } else {
+                return SizedBox.shrink();
+                }
               },
-            ),
-          ),
-      ],
-    );
-  }
-}
+           ),
+         ],
+       );
+     }
+   }
