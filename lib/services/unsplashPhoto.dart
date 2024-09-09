97477b3fa6577ce3c71e44ede6520db0a6be00ea
Ivy <ivyhsu0919@gmail.com>
2024-09-09 16:21:53 +0800

    creating unsplashAPI file

--- lib/services/unsplashPhoto.dart
+++ lib/services/unsplashPhoto.dart
@@ -2,37 +2,36 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:math';
 
-class RestaurantList {
+class PhotoCreator {
 
-  final List<String> restaurants = ['Italian', 'Chinese', 'Mexican', 'Indian'];
-  List<String> imageUrls = [];
-  bool isLoading = true;
+  Future<String?> getRandomImageUrl(String query) async {
+ 
+  var request = http.Request(
+    'GET',
+    Uri.parse(
+        'https://api.unsplash.com/search/photos?query=$query&client_id=YOUR_ACCESS_KEY&per_page=30')
+  );
 
+  http.StreamedResponse response = await request.send();
 
-  Future<void> _loadImages() async {
-    try {
-      for (String restaurant in restaurants) {
-        String imageUrl = await getRandomImageUrl('$restaurant restaurant');
-        imageUrls.add(imageUrl);
-      }
+  if (response.statusCode == 200) {
+    String responseBody = await response.stream.bytesToString();
+    final data = jsonDecode(responseBody);
+    final List<dynamic> results = data['results'];
 
-    } catch (e) {
-      print('Error loading images: $e');
-    }
-  }
-
-  Future<String> getRandomImageUrl(String query) async {
-    final apiKey = dotenv.env['unsplashApiKey'];
-    final url = Uri.parse('https://api.unsplash.com/photos/random?query=$query&client_id=$apiKey');
-
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      return data['urls']['regular'];
+    if (results.isNotEmpty) {
+      final randomIndex = Random().nextInt(results.length);
+      final imageUrl = results[randomIndex]['urls']['small'];
+      return imageUrl;
     } else {
-      throw Exception('Failed to load image');
+      print('No images found for the query.');
+      return null;
     }
+  } else {
+    print('Failed to load images: ${response.reasonPhrase}');
+    return null;
   }
+}
 }
