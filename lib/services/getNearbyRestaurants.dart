964ee15250e8082cc7eede2d6a371fa1ee26a1dd
here-comes-ivy <139777954+here-comes-ivy@users.noreply.github.com>
2024-09-10 11:53:51 +0800

    Updateing fetch photoUrl

--- lib/services/getNearbyRestaurants.dart
+++ lib/services/getNearbyRestaurants.dart
@@ -86,7 +86,7 @@ class NearbyRestaurantData {
             if (place['photos'].isNotEmpty) {
               String photoName = place['photos'][0]['name'] as String? ?? '';
               if (photoName.isNotEmpty) {
-                return 'https://places.googleapis.com/v1/$photoName/media?key=googApikey&maxHeightPx=$maxHeight&maxWidthPx=$maxWidth';
+                return 'https://places.googleapis.com/v1/$photoName/media?maxHeightPx=$maxHeight&maxWidthPx=$maxWidth&key=googApikey';
               }
             }
             return 'https://images.unsplash.com/photo-1514933651103-005eec06c04b?q=80&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'; // 返回空字符串或者默认图片 URL
