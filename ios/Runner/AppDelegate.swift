35d64613b50f92a4533aa731423d68d576c38b3e
Ivy <ivyhsu0919@gmail.com>
2024-09-24 08:40:23 +0800

    updating apikey

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,18 +1,37 @@
+// AppDelegate.swift
+
 import Flutter
 import UIKit
-import GoogleMaps                                          // Add this import
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-
-    // TODO: Add your Google Maps API key
-    GMSServices.provideAPIKey("AIzaSyA1s_Y3aQOLpynqLBTTG50s0d8QeERkKK4")               // Add this line
-
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let envChannel = FlutterMethodChannel(name: "com.yourcompany.env",
+                                              binaryMessenger: controller.binaryMessenger)
+        
+        envChannel.setMethodCallHandler({
+            (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            if call.method == "getGoogleMapsApiKey" {
+                if let apiKey = Bundle.main.object(forInfoDictionaryKey: "googApikey") as? String {
+                    GMSServices.provideAPIKey(apiKey)
+                    result(apiKey)
+                } else {
+                    result(FlutterError(code: "UNAVAILABLE",
+                                        message: "Google Maps API Key not found",
+                                        details: nil))
+                }
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        })
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
